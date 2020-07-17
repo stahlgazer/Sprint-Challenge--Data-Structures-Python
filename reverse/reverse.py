b6bcdfd18b81a8d1b16be0c8045132c31c40dae7
Gavin S <stahlgavin@gmail.com>
2020-07-17 10:26:30 -0700

    update notes

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,9 +12,18 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+    def __str__(self):
+        print(self.value)
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.value)
+            temp = temp.next_node
 
     def add_to_head(self, value):
         node = Node(value)
@@ -39,4 +48,28 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        # no list exists
+        if self.head is None:
+            return None
+        # if node reaches the end / is only node
+        if node.get_next() == None:
+            # node becomes head
+            self.head = node
+            return
+
+        # else start recursively reversing
+        next_node = node.get_next()
+        self.reverse_list(next_node, node)
+
+        node.get_next().set_next(node)
+        node.set_next(None)
+
+LL = LinkedList()
+gav = LL.add_to_head('I')
+test = LL.add_to_head('AM')
+newhead = LL.add_to_head('REVERSED')
+LL.printList()
+LL.reverse_list(LL.head, LL.head.next_node)
+print("List Now Reversed: ")
+LL.printList()
+
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,13 +6,18 @@ class RingBuffer:
         self.oldest = 0
 
     def append(self, item):
+        # if not full
         if self.size < self.capacity:
+            # add to ring and increase size 1
             self.ring.append(item)
             self.size += 1
+        # if full
         elif self.size == self.capacity:
+            # replace oldest item
             self.ring[self.oldest] = item
+            # increment to update oldest to next oldest
             self.oldest += 1
-            ## reset to front once it reaches end
+            # reset to front once it reaches end
             if self.oldest == self.capacity:
                 self.oldest = 0
             
