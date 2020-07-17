fd512c82be35b25d0d1460e90d8d8cf38dbe6e62
Gavin S <stahlgavin@gmail.com>
2020-07-17 09:03:57 -0700

    ring + names

--- names/names.py
+++ names/names.py
@@ -13,10 +13,14 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+if len(names_1) == len(names_2):
+    duplicates.extend(set(names_1).intersection(names_2))
+else:    
+    duplicates = [x for x in names_1 if x in names_2]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.ring = []
+        self.size = 0
+        self.oldest = 0
 
     def append(self, item):
-        pass
-
+        if self.size < self.capacity:
+            self.ring.append(item)
+            self.size += 1
+        elif self.size == self.capacity:
+            self.ring[self.oldest] = item
+            self.oldest += 1
+            ## reset to front once it reaches end
+            if self.oldest == self.capacity:
+                self.oldest = 0
+            
     def get(self):
-        pass
+        return self.ring
